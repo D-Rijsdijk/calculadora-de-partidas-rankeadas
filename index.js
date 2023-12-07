4b700de2acb069d97a12cc55f372edeb7f3b6312
D-Rijsdijk <diana.rijsdijk09@gmail.com>
2023-12-06 21:20:57 -0400

    melhora da função

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-let victories = 1;
+let victories = 85;
 let defeats = 1;
 let rank;
 let balanceWins;
 
 function calculateRank (){
     balanceWins = victories - defeats;
+
+    if(balanceWins<=10){
+        rank = "ferro";
+    }else if (balanceWins>=11 && balanceWins<=20){
+        rank = "bronze";
+    }else if(balanceWins>=21 && balanceWins<=50){
+        rank = "prata";
+    }else if(balanceWins>=51 && balanceWins<=80){
+        rank = "ouro";
+    }else if(balanceWins>=81 && balanceWins<=90){
+        rank = "diamante";
+    }else if(balanceWins>=91 && balanceWins<=100){
+        rank = "lendário";
+    }else if(balanceWins>=101){
+        rank = "imortal";
+    }
 }
 
 calculateRank ()
-
-if(balanceWins<=10){
-    rank = "ferro";
-}else if (balanceWins>=11 && balanceWins<=20){
-    rank = "bronze";
-}else if(balanceWins>=21 && balanceWins<=50){
-    rank = "prata";
-}else if(balanceWins>=51 && balanceWins<=80){
-    rank = "ouro";
-}else if(balanceWins>=81 && balanceWins<=90){
-    rank = "diamante";
-}else if(balanceWins>=91 && balanceWins<=100){
-    rank = "lendário";
-}else if(balanceWins>=101){
-    rank = "imortal";
-}
 
 console.log("O herói tem um aproveitamento de " +balanceWins+ " batalhas e está no nível de " +rank)
